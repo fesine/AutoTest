d2854e5c677a5e53dc1024f6e9007425fec7c6a7
Fesine <240944773@qq.com>
2018-05-19 17:29:11 +0800

    依赖用例测试

---
+++ chapter5/src/main/java/com/fesine/testng/DependTest.java
@@ -0,0 +1,26 @@
+package com.fesine.testng;
+
+import org.testng.annotations.Test;
+
+/**
+ * @description: 类描述
+ * @author: Fesine
+ * @createTime:2018/5/19
+ * @update:修改内容
+ * @author: Fesine
+ * @updateTime:2018/5/19
+ */
+public class DependTest {
+
+    @Test
+    public void test1(){
+        System.out.println("test1 run");
+        throw new RuntimeException();
+    }
+
+    @Test(dependsOnMethods = {"test1"})
+    public void test2(){
+        System.out.println("test2 run");
+    }
+
+}
