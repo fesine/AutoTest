840aa96cd89f5e4643a8442196d2940132108f6d
Fesine <240944773@qq.com>
2018-05-21 20:30:51 +0800

    第10学习post请求。

---
+++ chapter10/src/main/java/com/fesine/autotest/bean/User.java
@@ -0,0 +1,21 @@
+package com.fesine.autotest.bean;
+
+import lombok.Data;
+
+/**
+ * @description: 类描述
+ * @author: Fesine
+ * @createTime:2018/5/21
+ * @update:修改内容
+ * @author: Fesine
+ * @updateTime:2018/5/21
+ */
+@Data
+public class User {
+
+    private String username;
+    private String password;
+    private String name;
+    private int age;
+    private String sex;
+}
--- chapter10/src/main/java/com/fesine/autotest/server/MyPostMethod.java
+++ chapter10/src/main/java/com/fesine/autotest/server/MyPostMethod.java
@@ -1,14 +1,14 @@
 package com.fesine.autotest.server;
 
+import com.fesine.autotest.bean.User;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.Objects;
 
 /**
  * @description: 类描述
@@ -20,16 +20,16 @@ import javax.servlet.http.HttpServletResponse;
  */
 @RestController
 @RequestMapping("/v1")
-@Api(value = "/",description = "这是我所有的post请求方法")
+@Api(value = "/", description = "这是我所有的post请求方法")
 public class MyPostMethod {
 
     private Cookie cookie;
 
     @PostMapping("/login")
-    @ApiOperation(value = "登录接口，成功后获取cookies",httpMethod = "POST")
+    @ApiOperation(value = "登录接口，成功后获取cookies", httpMethod = "POST")
     public String login(HttpServletResponse response,
-                        @RequestParam(value = "username", required = true) String username,
-                        @RequestParam(value = "password", required = true) String password) {
+                        @RequestParam(value = "username") String username,
+                        @RequestParam(value = "password") String password) {
 
         if (username.equals("fesine") && password.equals("123456")) {
             cookie = new Cookie("login", "true");
@@ -39,5 +39,32 @@ public class MyPostMethod {
         return "用户名或密码输入不正确!";
     }
 
-    //TODO 实现获取cookie，并进行登录验证，返回用户信息
+    /**
+     * 实现获取cookie，并进行登录验证，返回用户信息
+     *
+     * @param request
+     * @param u
+     * @return
+     */
+    @PostMapping("/getUserList")
+    @ApiOperation(value = "获取用户列表", httpMethod = "POST")
+    public Object getUserList(HttpServletRequest request, @RequestBody User u) {
+        User user;
+        Cookie[] cookies = request.getCookies();
+        if(Objects.isNull(cookies)){
+            return "cookies信息不存在";
+        }
+        for (Cookie c : cookies) {
+            if (c.getName().equals("login") && c.getValue().equals("true") && u.getUsername()
+                    .equals("fesine") && u.getPassword().equals("123456")) {
+                user = new User();
+                user.setName("soly");
+                user.setAge(18);
+                user.setSex("male");
+                return user;
+            }
+        }
+        return "参数不合法！";
+    }
+
 }
